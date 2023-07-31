b937a9f419b807839941987e3c43dce6c48b0341
bilel4449 <bileltrain44@gmail.com>
2023-07-31 23:35:00 +0100

    ajouter des comontaires

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   state={
-    Fullname:'Beckham',
+    Fullname:'David Beckham',
     bio:'Ancien joueur footbool,jouai a equipe PSG,AC Milan et Real Madrid',
     image:'https://assets-fr.imgfoot.com/media/cache/1200x1200/david-beckham-2122-62600b8f74455.jpg',
     profission:'Joueur FootBall',
     show:true,
     count:0
   };
+  // fonction pour compteur
   handlePlus=()=>this.setState({count:this.state.count+1})
+  // fonction pour bouton
  handleShow=()=>this.setState({show: !this.state.show})
  componentDidMount() {
   console.log("componentDidMount")
@@ -25,6 +27,7 @@ export default class App extends Component {
         <button onClick={this.handleShow} className='btn'>Show</button>
         {this.state.show &&(
           <>
+          {/* appelle les elements de state */}
         <h3 className='titre'>{this.state.Fullname}</h3>
         <h3 className='titre'>{this.state.bio}</h3>
         <img className='img' src={this.state.image} alt="" />
